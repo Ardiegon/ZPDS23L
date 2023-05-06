42239122326a5f576a97fb6b14d4dcd15dcdee17
Mateusz Palczuk <mateusz.palczuk@gmail.com>
2023-05-06 15:02:58 +0200

    Add classes to denoising package

---
+++ src/denoising/__init__.py
@@ -0,0 +1,3 @@
+from src.denoising.denoising_model_interface import DenoisingModelInterface
+from src.denoising.denoising_cv2 import DenoisingCV2
+from src.denoising.denoising_unet import DenoisingUNet
